4659038e2f66df3b81b6448bb6de7b0b846e09fe
vincent <song374561@gmail.com>
2014-10-15 08:47:00 +0800

    database plugin from DatabaseInterface (https://github.com/chivincent/php/DatabaseInterface)

---
+++ plugin/php/config.php
@@ -0,0 +1,22 @@
+<?php
+	define('DB_NAME','test');
+	//Database Name
+	//(e.g. "demo")
+
+	define('DB_USER','tester');
+	//Database User
+	//(e.g. "root")
+
+	define('DB_PASS','testerpass');
+	//Database User Password
+	//(e.g. "rootpass")
+
+	define('DB_HOST','localhost');
+	//Database Hostserver
+	//(e.g. "localhost")
+
+	define('DB_CHAR','UTF8');
+	//Database Charset
+	//(e.g. "UTF8")
+
+?>
