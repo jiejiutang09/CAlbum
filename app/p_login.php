b1c8f177474b4ab4b29b50008c8c921bc926b1d1
vincent <song374561@gmail.com>
2014-10-21 00:19:19 +0800

    password verify, sql dump

--- app/p_login.php
+++ app/p_login.php
@@ -11,12 +11,7 @@
 
 	$data = $db->get('user','*','username',$_POST['username']);
 
-	$cost = 8;
-	$salt = chr(13).chr(25).chr(26).chr(30).chr(12).chr(17).chr(15).chr(5).chr(8).chr(3).chr(9).chr(1).chr(11).chr(41).chr(53).chr(58).chr(4).chr(32).chr(23).chr(21).chr(62).chr(57);
-	$username = $_POST['username'];
-	$password = password_hash($_POST['password'],PASSWORD_BCRYPT,['cost'=>$cost,'salt'=>$salt]);
-
-	if($data['username'] === $username && $data['password'] === $password)
+	if($data['username'] === $username && password_verify($_POST['password'],$data['password']))
 	{
 		if(!session_id())
 			session_start();
--- sql/dump.sql
+++ sql/dump.sql
@@ -1,19 +1,28 @@
--- phpMyAdmin SQL Dump
--- version 4.2.10
--- http://www.phpmyadmin.net
+/*
+ Navicat MySQL Data Transfer
 
+ Source Server         : localhost_root
+ Source Server Type    : MySQL
+ Source Server Version : 50538
+ Source Host           : localhost
+ Source Database       : calbum
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+ Target Server Type    : MySQL
+ Target Server Version : 50538
+ File Encoding         : utf-8
 
---
--- Database: `calbum`
---
+ Date: 10/20/2014 22:19:14 PM
+*/
 
--- --------------------------------------------------------
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
+-- ----------------------------
+--  Table structure for `language`
+-- ----------------------------
+DROP TABLE IF EXISTS `language`;
 CREATE TABLE `language` (
-`lid` int(5) NOT NULL,
+  `lid` int(5) NOT NULL AUTO_INCREMENT,
   `language` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
   `username` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -29,58 +38,86 @@ CREATE TABLE `language` (
   `hi` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `submit` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `forgetemail` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `home` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL
+  `home` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `systemsetting` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `title` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `subtitle` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `maintancemode` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lang` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `accountsetting` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `albumsetting` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `newalbum` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `uploadpic` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `delalbum` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `editalbum` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `loginerror` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `No` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `time` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `useragent` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`lid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- ----------------------------
+--  Records of `language`
+-- ----------------------------
+BEGIN;
+INSERT INTO `language` VALUES ('1', 'en', 'Username', 'Password', 'Email', 'Login', 'Logout', 'New Password', 'Confirm Newpassword', 'To change user data, please enter the password first', null, 'Save', 'Forgot Password', 'Hi, ', 'Submit', 'If you don\'t remember you Email, try to contact Website Manager', 'Home', 'Setting', 'Title', 'Sub-Title', 'Maintance Mode', 'Language', 'Account Setting', 'Album Setting', 'New Album', 'Upload', 'Delete Album', 'Edit Album', 'Login Log', 'No. ', 'Time', 'User Agent'), ('2', 'zh-tw', '帳號', '密碼', 'Email', '登入', '登出', '新密碼', '確認新密碼', '若要更動資料，您必須輸入密碼', 'Comic Album，由<a href=\"http://chivincent.github.io\">ChiVincent</a>開發之開源相簿專案<br>繁體中文翻譯由<a href=\"http://chivincent.github.io\">ChiVincent</a>製作', '更改資料', '忘記密碼', '安安，', '送出資料', '如果忘記您的Email，請聯繫網站管理員', '首頁', '系統設定', '網站名稱', '副標題', '維護模式', '語言', '帳號設定', '相簿設定', '新增相簿', '上傳圖片', '刪除相簿', '編輯相簿', '登入錯誤記錄', '編號', '時間', '使用者裝置');
+COMMIT;
 
-INSERT INTO `language` (`lid`, `language`, `username`, `password`, `email`, `login`, `logout`, `newpassword`, `confirm_newpassword`, `needtoenterpass`, `footer`, `modifydata`, `forgetpass`, `hi`, `submit`, `forgetemail`, `home`) VALUES
-(1, 'en', 'Username', 'Password', 'Email', 'Login', 'Logout', 'New Password', 'Confirm Newpassword', 'To change user data, please enter the password first', NULL, 'Save', 'Forgot Password', 'Hi, ', 'Submit', 'If you don''t remember you Email, try to contact Website Manager', 'Home'),
-(2, 'zh-tw', '帳號', '密碼', 'Email', '登入', '登出', '新密碼', '確認新密碼', '若要更動資料，您必須輸入密碼', 'Comic Album，由<a href="http://chivincent.github.io">ChiVincent</a>開發之開源相簿專案<br>繁體中文翻譯由<a href="http://chivincent.github.io">ChiVincent</a>製作', '更改資料', '忘記密碼', '安安，', '送出資料', '如果忘記您的Email，請聯繫網站管理員', '首頁');
+-- ----------------------------
+--  Table structure for `log`
+-- ----------------------------
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE `log` (
+  `logid` bigint(30) NOT NULL AUTO_INCREMENT,
+  `time` datetime DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `HTTP_CLIENT_IP` varchar(100) DEFAULT NULL,
+  `HTTP_X_FORWARDED_FOR` varchar(100) DEFAULT NULL,
+  `HTTP_VIA` varchar(100) DEFAULT NULL,
+  `REMOTE_ADDR` varchar(100) DEFAULT NULL,
+  `ua` text,
+  PRIMARY KEY (`logid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
-
+-- ----------------------------
+--  Table structure for `system`
+-- ----------------------------
+DROP TABLE IF EXISTS `system`;
 CREATE TABLE `system` (
-`sid` bigint(20) NOT NULL,
+  `sid` bigint(20) NOT NULL AUTO_INCREMENT,
   `title` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `subtitle` varchar(200) DEFAULT NULL,
   `language` varchar(50) DEFAULT NULL,
-  `create` datetime DEFAULT NULL
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`sid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+--  Records of `system`
+-- ----------------------------
+BEGIN;
+INSERT INTO `system` VALUES ('1', 'Comic Album', 'Demo Site', 'zh-tw', '2014-10-20 20:55:36');
+COMMIT;
 
-INSERT INTO `system` (`sid`, `title`, `subtitle`, `language`, `create`) VALUES
-(1, 'Comic Album', 'Demo Site', 'zh-tw', '2014-10-19 13:30:48');
-
--- --------------------------------------------------------
-
-
+-- ----------------------------
+--  Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-`uid` int(5) NOT NULL,
+  `uid` int(5) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(60) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
-INSERT INTO `user` (`uid`, `username`, `password`, `email`) VALUES
-(3, 'root', '$2y$08$DRkaHgwRDwUIAwkBCyk1OeklLjRUR099rofxoT.c9tDJtBuzBi9l.', 'root@root');
+-- ----------------------------
+--  Records of `user`
+-- ----------------------------
+BEGIN;
+INSERT INTO `user` VALUES ('3', 'root', '$2y$08$DRkaHgwRDwUIAwkBCyk1OeklLjRUR099rofxoT.c9tDJtBuzBi9l.', 'root@root');
+COMMIT;
 
-
-ALTER TABLE `language`
- ADD PRIMARY KEY (`lid`);
-
-ALTER TABLE `system`
- ADD PRIMARY KEY (`sid`);
-
-ALTER TABLE `user`
- ADD PRIMARY KEY (`uid`);
-
-ALTER TABLE `language`
-MODIFY `lid` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
-
-ALTER TABLE `system`
-MODIFY `sid` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
-資料表 AUTO_INCREMENT `user`
-
-ALTER TABLE `user`
-MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+SET FOREIGN_KEY_CHECKS = 1;
